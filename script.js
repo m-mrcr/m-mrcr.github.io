7dd0c79218b1670cdcc8000bfad7076fcd8c014d
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2021-05-26 19:16:27 -0600

    Add menu

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 "use strict";
 
 window.addEventListener("load", () => {
+  darkMode();
+  textEffect();
+});
+
+function textEffect() {
   const container = document.querySelector("#first h1");
   if (!container) return;
-
   const classNames = ["blue", "red", "pink", "yellow", "purple"];
-
   const rawString = container.textContent;
-
   container.innerHTML = "";
-
   for (let i = 0; i < rawString.length; i++) {
     const character = rawString[i];
     const spanny = document.createElement("span");
     spanny.textContent = character;
-
     spanny.classList.add(classNames[i % classNames.length]);
-
     container.appendChild(spanny);
   }
-});
+}
 
-// Wait for document to load
-document.addEventListener("DOMContentLoaded", function (event) {
+function darkMode() {
   document.documentElement.setAttribute("data-theme", "light");
-
-  // Get our button switcher
-  var themeSwitcher = document.getElementById("theme-switcher");
-
-  // When our button gets clicked
+  const themeSwitcher = document.getElementById("theme-switcher");
   themeSwitcher.onclick = function () {
-    // Get the current selected theme, on the first run
-    // it should be `light`
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    // Switch between `dark` and `light`
-    var switchToTheme = currentTheme === "dark" ? "light" : "dark";
-
-    // Set our currenet theme to the new one
+    let currentTheme = document.documentElement.getAttribute("data-theme");
+    const switchToTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", switchToTheme);
   };
+}
+
+document.querySelector(".menu-button").addEventListener("click", () => {
+  document.querySelector(".circular-menu").classList.toggle("open");
 });
+
+const items = document.querySelectorAll(".menu a");
+const thetaDelta = Math.PI / 2 / (items.length - 1);
+const radius = 120;
+for (let i = 0; i < items.length; i++) {
+  const item = items[i];
+  const theta = i * thetaDelta - Math.PI / 2;
+  const x = Math.cos(theta) * radius;
+  const y = Math.sin(theta) * radius;
+  item.style.transform = `translate(${x}px, ${y}px)`;
+}
